05ffb0fd509b88f84e2b92d01669d22ddb0e8cb8
y karabulut <455198@student.fontys.nl>
2025-04-10 12:15:25 +0200

    one line joke assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+const oneLinerJoke = require('one-liner-joke');
+// Get a random joke
+const getRandomJoke = oneLinerJoke.getRandomJoke();
+// Log the joke to the console
+console.log(getRandomJoke.body);
+
